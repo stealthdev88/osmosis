stealthdev88/osmosis

commit d8b4f25863abd79ba2d36ce63cf0b839360da454

Updated the getWriteableInstance methods to use a cloning constructor instead of specifying all arguments to a normal constructor.

core/src/org/openstreetmap/osmosis/core/domain/v0_6/Relation.java
@@ -152,6 +152,19 @@ public class Relation extends Entity implements Comparable<Relation> {
 	/**
 	 * Creates a new instance.
 	 * 
+	 * @param originalRelation
+	 *            The relation to clone from.
+	 */
+	public Relation(Relation originalRelation) {
+		super(originalRelation);
+		
+		this.members = new ArrayList<RelationMember>(members);
+	}
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
 	 * @param sr
 	 *            The store to read state from.
 	 * @param scr
@@ -331,8 +344,7 @@ public class Relation extends Entity implements Comparable<Relation> {
 	@Override
 	public Relation getWriteableInstance() {
 		if (isReadOnly()) {
-			return new Relation(getId(), getVersion(), getTimestampContainer(), getUser(), getChangesetId(), getTags(),
-					members);
+			return new Relation(this);
 		} else {
 			return this;
 		}

core/src/org/openstreetmap/osmosis/core/domain/v0_6/Way.java
@@ -153,6 +153,19 @@ public class Way extends Entity implements Comparable<Way> {
 	/**
 	 * Creates a new instance.
 	 * 
+	 * @param originalWay
+	 *            The way to clone from.
+	 */
+	public Way(Way originalWay) {
+		super(originalWay);
+		
+		this.wayNodes = new ArrayList<WayNode>(wayNodes);
+	}
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
 	 * @param sr
 	 *            The store to read state from.
 	 * @param scr
@@ -341,8 +354,7 @@ public class Way extends Entity implements Comparable<Way> {
 	@Override
 	public Way getWriteableInstance() {
 		if (isReadOnly()) {
-			return new Way(getId(), getVersion(), getTimestampContainer(), getUser(), getChangesetId(), getTags(),
-					wayNodes);
+			return new Way(this);
 		} else {
 			return this;
 		}

core/src/org/openstreetmap/osmosis/core/domain/v0_6/CommonEntityData.java
@@ -465,12 +465,16 @@ public class CommonEntityData implements Storeable {
 
 
 	/**
-	 * Returns a writeable instance of this object. If the object is read-only a clone is created,
-	 * if it is already writeable then this object is returned.
+	 * Returns a writable instance of this object. If the object is read-only a clone is created,
+	 * if it is already writable then this object is returned.
 	 * 
-	 * @return A writeable instance of this object.
+	 * @return A writable instance of this object.
 	 */
 	public CommonEntityData getWriteableInstance() {
-		return new CommonEntityData(id, version, timestampContainer, user, changesetId, tags);
+		if (isReadOnly()) {
+			return new CommonEntityData(id, version, timestampContainer, user, changesetId, tags);
+		} else {
+			return this;
+		}
 	}
 }

core/src/org/openstreetmap/osmosis/core/domain/v0_6/Entity.java
@@ -113,7 +113,18 @@ public abstract class Entity implements Storeable {
 	 *            The data to store in the entity. This instance is used directly and is not cloned.
 	 */
 	public Entity(CommonEntityData entityData) {
-		this.entityData = entityData;
+		this.entityData = entityData.getWriteableInstance();
+	}
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param originalEntity
+	 *            The entity to clone from.
+	 */
+	public Entity(Entity originalEntity) {
+		this.entityData = originalEntity.entityData.getWriteableInstance();
 	}
 	
 	

core/src/org/openstreetmap/osmosis/core/domain/v0_6/Node.java
@@ -144,6 +144,19 @@ public class Node extends Entity implements Comparable<Node> {
 
 		init(latitude, longitude);
 	}
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param originalNode
+	 *            The node to clone from.
+	 */
+	private Node(Node originalNode) {
+		super(originalNode);
+		
+		init(originalNode.latitude, originalNode.longitude);
+	}
 
 
 	/**
@@ -337,8 +350,7 @@ public class Node extends Entity implements Comparable<Node> {
 	@Override
 	public Node getWriteableInstance() {
 		if (isReadOnly()) {
-			return new Node(getId(), getVersion(), getTimestampContainer(), getUser(), getChangesetId(), getTags(),
-					latitude, longitude);
+			return new Node(this);
 		} else {
 			return this;
 		}

commit e372581781f6b10d3728f30bf58d7ddfd23cd221

Fixed old regression in Way and Relation cloning.
The Way and Relation cloning was broken in revision
d8b4f25863abd79ba2d36ce63cf0b839360da454 at Mon May 9 13:44:50 2011
+0000.  Cloning only occurs when attempting to get a writeable copy of a
read-only entity.  In normal processing, the typical way an object gets
marked read-only is when using a --tee task.  A writeable object is only
required by tasks modifying entities such as --bounding-box with
the clipIncompleteEntities=true option.

The cloning was using the incorrect source object when trying to clone
the way nodes, and relation members lists and accessing a null object.

core/src/main/java/org/openstreetmap/osmosis/core/domain/v0_6/Relation.java
@@ -162,7 +162,7 @@ public class Relation extends Entity implements Comparable<Relation> {
 	private Relation(Relation originalRelation) {
 		super(originalRelation);
 		
-		this.members = new ArrayList<RelationMember>(members);
+		this.members = new ArrayList<RelationMember>(originalRelation.members);
 	}
 	
 	

core/src/main/java/org/openstreetmap/osmosis/core/domain/v0_6/Way.java
@@ -163,7 +163,7 @@ public class Way extends Entity implements Comparable<Way> {
 	private Way(Way originalWay) {
 		super(originalWay);
 		
-		this.wayNodes = new ArrayList<WayNode>(wayNodes);
+		this.wayNodes = new ArrayList<WayNode>(originalWay.wayNodes);
 	}
 	
 	

core/src/test/java/org/openstreetmap/osmosis/core/domain/v0_6/CloneTest.java
@@ -1,0 +1,87 @@
+// This software is released into the Public Domain.  See copying.txt for details.
+package org.openstreetmap.osmosis.core.domain.v0_6;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Verifies that read-only entities can be cloned.
+ * 
+ * @author Brett Henderson
+ * 
+ */
+public class CloneTest {
+	/**
+	 * Node cloning test.
+	 */
+	@Test
+	public void testNodeClone() {
+		// Build the original entity.
+		List<Tag> tags = new ArrayList<Tag>();
+		tags.add(new Tag("myKey", "myValue"));
+		Node entity = new Node(new CommonEntityData(1, 2, new Date(0), OsmUser.NONE, 3, tags), 4, 5);
+
+		// Cloning a writeable object should return the original object.
+		Assert.assertSame("Entity was cloned", entity, entity.getWriteableInstance());
+
+		// Get a cloned entity.
+		entity.makeReadOnly();
+		Node clonedEntity = entity.getWriteableInstance();
+
+		// Make sure we weren't assigned the original entity.
+		Assert.assertNotSame("Entity was not cloned", entity, clonedEntity);
+	}
+
+
+	/**
+	 * Way cloning test.
+	 */
+	@Test
+	public void testWayClone() {
+		// Build the original entity.
+		List<Tag> tags = new ArrayList<Tag>();
+		tags.add(new Tag("myKey", "myValue"));
+		List<WayNode> wayNodes = new ArrayList<WayNode>();
+		wayNodes.add(new WayNode(1));
+		Way entity = new Way(new CommonEntityData(1, 2, new Date(0), OsmUser.NONE, 3, tags), wayNodes);
+
+		// Cloning a writeable object should return the original object.
+		Assert.assertSame("Entity was cloned", entity, entity.getWriteableInstance());
+
+		// Get a cloned entity.
+		entity.makeReadOnly();
+		Way clonedEntity = entity.getWriteableInstance();
+
+		// Make sure we weren't assigned the original entity.
+		Assert.assertNotSame("Entity was not cloned", entity, clonedEntity);
+	}
+
+
+	/**
+	 * Relation cloning test.
+	 */
+	@Test
+	public void testRelationClone() {
+		// Build the original entity.
+		List<Tag> tags = new ArrayList<Tag>();
+		tags.add(new Tag("myKey", "myValue"));
+		List<RelationMember> members = new ArrayList<RelationMember>();
+		members.add(new RelationMember(1, EntityType.Node, "myRole"));
+		Relation entity = new Relation(new CommonEntityData(1, 2, new Date(0), OsmUser.NONE, 3, tags), members);
+
+		// Cloning a writeable object should return the original object.
+		Assert.assertSame("Entity was cloned", entity, entity.getWriteableInstance());
+
+		// Get a cloned entity.
+		entity.makeReadOnly();
+		Relation clonedEntity = entity.getWriteableInstance();
+
+		// Make sure we weren't assigned the original entity.
+		Assert.assertNotSame("Entity was not cloned", entity, clonedEntity);
+	}
+}